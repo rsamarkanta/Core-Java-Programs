c37668df3a7cab058ac81d35e483a2ecbe7c3b6b
TechSamar <TechSamar@192.168.1.42>
2023-03-20 13:48:14 +0530

    Add two numbers (without user input)

---
+++ 02-AddTwoNumbers/src/com/ts/add/AddTwoNumbers.java
@@ -0,0 +1,19 @@
+package com.ts.add;
+
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+		// initialize integer variables
+		int a=24;
+		int b=30;
+		int c=0;
+		
+		//print all the value of variables
+		System.out.println("a = "+a+", b = "+b+", c = "+c);
+		
+		//calculate a+b and store its value to c
+		c=a+b;
+		System.out.println("c = a + b ="+c);
+	}
+
+}
