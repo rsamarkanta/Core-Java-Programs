374a6211b85759a46d0c11a433fefe7c24528eac
TechSamar <TechSamar@192.168.1.42>
2023-03-20 13:45:41 +0530

    Hello World Program

---
+++ 01-HelloWorld/src/com/ts/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.ts.hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, Welcome to TechSamar's Java Programming World");
+	}
+	
+}
