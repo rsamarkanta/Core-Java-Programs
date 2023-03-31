595b28f40d045911aefaefb2ada0359171921c40
TechSamar <TechSamar@192.168.1.58>
2023-03-31 10:17:52 +0530

    Star Pattern Program 3

---
+++ 64-StarPatternProgram-3/src/com/ts/pattern/StarPattern1.java
@@ -0,0 +1,17 @@
+package com.ts.pattern;
+
+public class StarPattern1 {
+
+	public static void main(String[] args) {
+		int i, j;
+		for (i = 1; i <= 6; i++) {// rows
+			for (j = 1; j <= 6; j++) {// columns
+				// print *
+				System.out.print("*");
+			}
+			// to create a new line
+			System.out.println();
+		}
+	}
+
+}
