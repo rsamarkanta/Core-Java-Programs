4a6752e0b6d3060b1c491def9087fb2a9047c8b3
TechSamar <TechSamar@192.168.1.44>
2023-04-09 14:46:49 +0530

    Queue Program

---
+++ 94-QueueProgram/src/com/ts/collection/QueueArrayImplementation.java
@@ -0,0 +1,42 @@
+package com.ts.collection;
+
+public class QueueArrayImplementation {
+	public static void main(String[] args) {
+		// Create a queue of capacity 4
+		Queue q = new Queue(4);
+
+		System.out.println("Initial Queue:");
+		// print Queue elements
+		Queue.queueDisplay();
+
+		// inserting elements in the queue
+		q.queueEnqueue(10);
+		q.queueEnqueue(30);
+		q.queueEnqueue(50);
+		q.queueEnqueue(70);
+
+		// print Queue elements
+		System.out.println("Queue after Enqueue Operation:");
+		q.queueDisplay();
+
+		// print front of the queue
+		q.queueFront();
+
+		// insert element in the queue
+		q.queueEnqueue(90);
+
+		// print Queue elements
+		q.queueDisplay();
+
+		q.queueDequeue();
+		q.queueDequeue();
+		System.out.printf("\nQueue after two dequeue operations:");
+
+		// print Queue elements
+		q.queueDisplay();
+
+		// print front of the queue
+		q.queueFront();
+
+	}
+}
