fff096075c69e4dae9ffc4017390422f119d8f13
TechSamar <TechSamar@192.168.1.44>
2023-04-10 18:00:15 +0530

    Encapsulation Example

---
+++ 98-EncapsulationExamples/src/com/ts/oops/EncapsulationExamples.java
@@ -0,0 +1,59 @@
+package com.ts.oops;
+
+//A Student class which is a fully encapsulated class.  
+//It has private data members and getter and setter methods.
+class Student {
+	// private data members
+	private int roll;
+	private String name;
+	private String address;
+
+	// public getter and setter methods
+	public int getRoll() {
+		return roll;
+	}
+
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [roll=" + roll + ", name=" + name + ", address=" + address + "]";
+	}
+
+}
+
+public class EncapsulationExamples {
+	public static void main(String[] args) {
+
+		// Create object
+		Student st = new Student();
+
+		// //setting values through setter methods
+		st.setRoll(101);
+		st.setName("Ramesh");
+		st.setAddress("Pune");
+
+		// getting values through getter methods
+		System.out.println(
+				" Roll no : " + st.getRoll() + "\n Name : " + st.getName() + "\n Address : " + st.getAddress());
+
+	}
+}
